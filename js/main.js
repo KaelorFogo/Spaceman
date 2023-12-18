902809bceb94fde8a07a40b3bbbdcc81a00f027f
KaelorFogo <kfogo19@gmail.com>
2023-12-18 11:01:05 -0700

    Spaceman working

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
  /*----- constants -----*/
 const words ={
-  movies:['interstellar', 'spider man', 'star wars', 'good will hunting'],
-  music:['drake', 'mac miller', 'kanye west', 'ice cube'],
-  geography: ['united states', 'france', 'japan', 'australia'],
-  animals: ['kangaroo', 'bald eagle', 'beaver', 'treefrog']
+  movies:['interstellar', 'spider man', 'star wars', 'good will hunting', 'the breakfast club', 'inception'],
+  music:['drake', 'mac miller', 'kanye west', 'ice cube', 'lady gaga', 'ariana grande'],
+  geography: ['united states', 'france', 'japan', 'australia', 'columbia', 'Czechoslovakia'],
+  animals: ['kangaroo', 'bald eagle', 'beaver', 'treefrog', 'python', 'dolphin', 'blob fish']
 }
 
   /*----- state variables -----*/
@@ -11,21 +11,28 @@ let board = [];
 let playerBoard = [];
 let guessWord;
 let winner = null;
+let guessesToGo = 0;
+
 
   /*----- cached elements  -----*/
   const boardEl = document.getElementById('board');
+  const keysEl = document.querySelector('#keys');
+  const winEl = document.getElementById('win-message');
+  const imgEl = document.getElementById('img');
 
   /*----- event listeners -----*/
   document.querySelector('.dropdown-content').addEventListener('click', handleWord);
-  document.querySelector('#keys').addEventListener('click', handleKeys);
+  keysEl.addEventListener('click', handleKeys);
 
 
 
   /*----- functions -----*/
  function handleWord(evt){
+  winner = null;
   guessWord = '';
   playerBoard = [];
   board = [];
+  winEl.innerHTML = '';
   boardEl.innerText = '';
   const categorySel = evt.target.innerText.toLowerCase();
   const rndWord = Math.floor(Math.random() * words[categorySel].length);
@@ -35,11 +42,16 @@ let winner = null;
 
  function handleKeys(evt){
   let keySelect = evt.target.innerHTML;
+  let correctkey = 0;
   for(let i = 0; i < board.length; i++){
     if(board[i] === keySelect){
       let letIdx = i;
       playerBoard[letIdx] = keySelect;
+      correctkey++;
     }
+  }
+  if(correctkey === 0){
+    guessesToGo++
   }
   render();
  }
@@ -57,20 +69,40 @@ let winner = null;
       }
     }
     render();
+    imgEl.style.visibility = 'visible';
+    keysEl.style.visibility = 'visible';
   }
 
   function render(){
+    imgEl.src = `img/spaceman-${guessesToGo}.jpg`;
     renderBoard();
+    checkWin();
+    renderMessage();
+  }
+
+  function renderMessage() {
+    if(winner){
+      winEl.innerHTML = 'You Won! Select a new category to continue!';
+    }
+  }
+
+  function checkWin(){
+    let letLeft = 0;
+    playerBoard.forEach(numLeft =>{
+        if(numLeft === '_'){
+          letLeft++
+        }
+    })
+    if(letLeft === 0){
+      winner = true;
+    }
   }
 
   function renderBoard(){
-    console.log(boardEl.innerHTML)
     if(boardEl.innerHTML = "\n      "){
       for(let i = 0; i < playerBoard.length; i++){
       boardEl.innerHTML += playerBoard[i];
       }
     }
-
-    
   }
 
