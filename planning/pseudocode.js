7702506316ef54292ea3c7deb863e80f79a247a7
KaelorFogo <kfogo19@gmail.com>
2023-12-20 14:00:10 -0700

    planning folder

---
+++ planning/pseudocode.js
@@ -0,0 +1,14 @@
+/*
+0. initialize the game
+1. let the player choose an array of which catagory they want the word to come from 
+1.1 pick a random string from that array for the guess word
+
+2. render the board to show the number of letters in the string the player needs to guess 
+
+3. take an input of a char to see if that char is in the string
+3.1 if that char is not in th estring take off a life from the player
+
+4. if the player sucessfully gets the word with lives remaining render a message congratulating them
+4.1 if they player fails to guess the word before they run out of lives show them a loss screen 
+4.2 render a play again button when the game finishes to reset the game back to the player picking a category for their word
+*/
